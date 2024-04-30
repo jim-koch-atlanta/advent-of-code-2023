69231fe801a2c3f345395e369fc3628e70d8c7fa
Jim <jimkoch@gmail.com>
2024-04-29 23:08:53 -0400

    Advent of Code: Day 4, Part 2.

--- day04/src/Main.java
+++ day04/src/Main.java
@@ -3,6 +3,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
     // Function to check if an array contains an element
@@ -15,7 +17,7 @@ public class Main {
         return false;
     }
 
-    public static void main(String[] args) {
+    public static void main0(String[] args) {
         String fileName = "src/input.txt"; // Specify the path to your text file
 
         int total = 0;
@@ -56,5 +58,75 @@ public class Main {
         }
 
         System.out.println("Total: " + total);
+    }
+
+    public static Integer getMatches(String line) {
+        String[] colonSplit = line.split(":");
+        String[] splits = colonSplit[1].split("\\|");
+        String[] strCardNumbers = splits[0].split("\\s");
+        String[] strMyNumbers = splits[1].split("\\s");
+
+        Integer[] cardNumbers = Arrays.stream(strCardNumbers)
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+
+        Integer[] myNumbers = Arrays.stream(strMyNumbers)
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+
+        Integer[] matchingNumbers = Arrays.stream(cardNumbers)
+                .filter(cardNumber -> contains(myNumbers, cardNumber))
+                .toArray(Integer[]::new);
+
+        return matchingNumbers.length;
+    }
+
+    public static void main(String[] args) {
+        String fileName = "src/input.txt"; // Specify the path to your text file
+
+        HashMap<Integer, Integer> Scratchcards = new HashMap<Integer, Integer>();
+        int totalScratchcards = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = br.readLine();
+            int currentLine = 0;
+
+            // Read lines from the file until the end is reached
+            while (line != null) {
+                System.out.println(line);
+                // How many scratchcards do I get of the current line?
+                int numberOfScratchcards = (Scratchcards.getOrDefault(currentLine, 0));
+                numberOfScratchcards++;
+
+                System.out.println("Number of scratchcards: " + numberOfScratchcards);
+
+                totalScratchcards += numberOfScratchcards;
+
+                System.out.println("Total scratchcards    : " + totalScratchcards);
+
+                // How many matches did I get on the current line?
+                int numberOfMatches = getMatches(line);
+                System.out.println("Number of matches     : " + numberOfMatches);
+                for (int i = 1; i <= numberOfMatches; i++) {
+                    // Update the number of scratchcards that I have available.
+                    int previousNumberOfScratchcards = Scratchcards.getOrDefault(currentLine + i, 0);
+                    int newNumberOfScratchcards = previousNumberOfScratchcards + numberOfScratchcards;
+                    Scratchcards.put(currentLine + i, newNumberOfScratchcards);
+                    System.out.printf("Scratchcards[%d]=[%d]\n", currentLine + i, newNumberOfScratchcards);
+                }
+
+                line = br.readLine();
+                currentLine++;
+
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+
+        System.out.println("Total Scratchcards: " + totalScratchcards);
     }
 }
