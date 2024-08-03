7b7aa9086296ebe8634f59fb497869f1cece2a11
Jim <jimkoch@gmail.com>
2024-08-03 14:04:51 -0400

    Advent of Code: Day 8, Part 2.

--- day08/src/Main.java
+++ day08/src/Main.java
@@ -1,9 +1,13 @@
+import org.w3c.dom.Node;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class Main {
+
+    static List<String> allNodes;
 
     static String directions;
 
@@ -15,6 +19,8 @@ public class Main {
 
     // We now will have a full list of current nodes. Track them as a list.
     static List<String> currentNodes;
+
+    static Map<String, NodeDestinations> nodeDestinationsMap;
 
     static long steps;
 
@@ -35,6 +41,7 @@ public class Main {
                 String leftNode = line.substring(7, 10);
                 String rightNode = line.substring(12, 15);
                 // System.out.printf("%s: %s, %s\n", currentNode, leftNode, rightNode);
+                allNodes.add(currentNode);
                 left.put(currentNode, leftNode);
                 right.put(currentNode, rightNode);
 
@@ -59,54 +66,83 @@ public class Main {
         }
     }
 
-    public static boolean makeMoves(char direction) {
-        boolean foundAllZ = true;
-        for (int i = 0; i < currentNodes.size(); i++) {
-            String currentNode = currentNodes.get(i);
-            currentNode = makeMove(currentNode, direction);
-            if (!currentNode.endsWith("Z")) {
-                foundAllZ = false;
-            }
-            currentNodes.set(i, currentNode);
-        }
+    public static void createNodeDestinationsMap() {
+        // We're going to pre-process the different Z's that you can reach
+        // from each starting node. This will reduce the processing needed
+        // while iterating.
+        for (String node : allNodes) {
+            String currentNode = node;
 
-        return foundAllZ;
-    }
-
-    public static void printCurrentState() {
-        System.out.printf("Current steps: %d\n", steps);
-        for (String currentNode : currentNodes) {
-            System.out.println(currentNode);
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        String fileName = "src/input.txt"; // Specify the path to your text file
-        left = new HashMap<>();
-        right = new HashMap<>();
-        currentNodes = new Vector<>();
-        steps = 0;
-
-        readFile(fileName);
-
-        // Let's go!
-        boolean foundAllZ = false;
-        while (!foundAllZ) {
+            int step = 0;
+            NodeDestinations nodeDestinations = new NodeDestinations();
             for (char direction : directions.toCharArray()) {
-                foundAllZ = makeMoves(direction);
-                steps++;
-
-                if (steps % 10000000 == 0) {
-                    System.out.printf("%d steps\n", steps);
+                currentNode = makeMove(currentNode, direction);
+                step++;
+                if (currentNode.charAt(2) == 'Z') {
+                    nodeDestinations.stepsToZNodes.add(step);
                 }
+            }
+            nodeDestinations.destination = currentNode;
+            nodeDestinationsMap.put(node, nodeDestinations);
+        }
+    }
 
-                if (foundAllZ) {
-                    System.out.printf("FOUND ALL Z AT %d steps\n", steps);
+    public static Integer findMatch() {
+        NodeDestinations nodeDestinationsZero = null;
+        List<NodeDestinations> nodeDestinationsList = new Vector<>();
+        for (String currentNode : currentNodes) {
+            if (nodeDestinationsZero == null) {
+                nodeDestinationsZero = nodeDestinationsMap.get(currentNode);
+            } else {
+                nodeDestinationsList.add(nodeDestinationsMap.get(currentNode));
+            }
+        }
+
+        boolean foundMatch = true;
+        assert nodeDestinationsZero != null;
+        Integer currentSteps = null;
+        for (Integer steps : nodeDestinationsZero.stepsToZNodes) {
+            foundMatch = true;
+            currentSteps = steps;
+            for (NodeDestinations nd : nodeDestinationsList) {
+                if (!nd.stepsToZNodes.contains(steps)) {
+                    foundMatch = false;
                     break;
                 }
             }
         }
 
+        if (foundMatch) {
+            return currentSteps;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String fileName = "src/input.txt"; // Specify the path to your text file
+        allNodes = new Vector<>();
+        left = new HashMap<>();
+        right = new HashMap<>();
+        currentNodes = new Vector<>();
+        nodeDestinationsMap = new HashMap<>();
+        steps = 0;
+
+        readFile(fileName);
+        int directionsLength = directions.length();
+        createNodeDestinationsMap();
+
+        Integer match = null;
+        do {
+            match = findMatch();
+            if (match != null) {
+                steps = steps + match;
+                break;
+            }
+
+            currentNodes.replaceAll(node -> nodeDestinationsMap.get(node).destination);
+            steps = steps + directionsLength;
+        } while (match == null);
         System.out.printf("\nSteps: %d", steps);
     }
 }
---
+++ day08/src/NodeDestinations.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.Vector;
+
+public class NodeDestinations {
+    List<Integer> stepsToZNodes;
+
+    String destination;
+
+    public NodeDestinations() {
+        stepsToZNodes = new Vector<>();
+    }
+}
