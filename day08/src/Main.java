046c9b2769140f28002ed7b260e03639b046d9a7
Jim <jimkoch@gmail.com>
2024-08-02 22:08:46 -0400

    Advent of Code: Day 8, Part 2: Brute force.

--- day08/src/Main.java
+++ day08/src/Main.java
@@ -12,6 +12,12 @@ public class Main {
 
     // What happens if you go right?
     static Map<String, String> right;
+
+    // We now will have a full list of current nodes. Track them as a list.
+    static List<String> currentNodes;
+
+    static long steps;
+
     public static void readFile(String fileName) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
@@ -28,14 +34,21 @@ public class Main {
                 String currentNode = line.substring(0, 3);
                 String leftNode = line.substring(7, 10);
                 String rightNode = line.substring(12, 15);
-                System.out.printf("%s: %s, %s\n", currentNode, leftNode, rightNode);
+                // System.out.printf("%s: %s, %s\n", currentNode, leftNode, rightNode);
                 left.put(currentNode, leftNode);
                 right.put(currentNode, rightNode);
+
+                if (currentNode.endsWith("A")) {
+                    System.out.printf("Starting node: %s\n", currentNode);
+                    currentNodes.add(currentNode);
+                }
                 line = br.readLine();
             }
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
         }
+
+        System.out.printf("\n\n");
     }
 
     public static String makeMove(String currentNode, char direction) {
@@ -46,23 +59,51 @@ public class Main {
         }
     }
 
+    public static boolean makeMoves(char direction) {
+        boolean foundAllZ = true;
+        for (int i = 0; i < currentNodes.size(); i++) {
+            String currentNode = currentNodes.get(i);
+            currentNode = makeMove(currentNode, direction);
+            if (!currentNode.endsWith("Z")) {
+                foundAllZ = false;
+            }
+            currentNodes.set(i, currentNode);
+        }
+
+        return foundAllZ;
+    }
+
+    public static void printCurrentState() {
+        System.out.printf("Current steps: %d\n", steps);
+        for (String currentNode : currentNodes) {
+            System.out.println(currentNode);
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         String fileName = "src/input.txt"; // Specify the path to your text file
         left = new HashMap<>();
         right = new HashMap<>();
+        currentNodes = new Vector<>();
+        steps = 0;
 
         readFile(fileName);
 
         // Let's go!
-        int steps = 0;
-        String currentNode = "AAA";
-        while (!currentNode.equals("ZZZ")) {
-            for (char c : directions.toCharArray()) {
-                System.out.printf("Current Node: %s, Move: %c\n", currentNode, c);
-                currentNode = makeMove(currentNode, c);
+        boolean foundAllZ = false;
+        while (!foundAllZ) {
+            for (char direction : directions.toCharArray()) {
+                foundAllZ = makeMoves(direction);
                 steps++;
-                if (currentNode.equals("ZZZ"))
+
+                if (steps % 10000000 == 0) {
+                    System.out.printf("%d steps\n", steps);
+                }
+
+                if (foundAllZ) {
+                    System.out.printf("FOUND ALL Z AT %d steps\n", steps);
                     break;
+                }
             }
         }
 
