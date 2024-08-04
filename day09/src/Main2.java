7085d1d6b6e25cb39f6da2cd37326ca16ae5e212
Jim <jimkoch@gmail.com>
2024-08-04 09:38:57 -0400

    Advent of Code: Day 9, Part 2.

---
+++ day09/src/Main2.java
@@ -0,0 +1,62 @@
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main2 {
+
+    static List<List<Integer>> lines;
+
+    public static void readFile(String fileName) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+            String line = br.readLine();
+            while (line != null) {
+                List<Integer> intLine = Arrays.stream(line.split(" ")).map(Integer::parseInt).toList();
+                lines.add(intLine);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+
+        System.out.print("\n\n");
+    }
+
+    public static Integer findNextValue(List<Integer> line) {
+        if (line.stream().anyMatch(i -> i != 0)) {
+            List<Integer> nextLine = new Vector<>();
+            Integer prevValue = null;
+            for (Integer value : line) {
+                if (prevValue == null) {
+                    prevValue = value;
+                } else {
+                    nextLine.add(value - prevValue);
+                    prevValue = value;
+                }
+            }
+
+            return (line.getFirst() - findNextValue(nextLine));
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        String fileName = "src/input.txt"; // Specify the path to your text file
+
+        lines = new Vector<>();
+        readFile(fileName);
+
+        Integer total = 0;
+        for (List<Integer> line : lines) {
+            Integer nextValue = findNextValue(line);
+            total += nextValue;
+        }
+
+        System.out.printf("Result: %d\n", total);
+    }
+}
